d3452628557e997547faa77483207bea9c01919d
tan <1445763190@qq.com>
2021-12-12 15:39:56 +0800

    [示例代码] jps 工具类优化依赖后的相应处理

--- HJPA/src/main/java/cn/tannn/hjpa/vo/CommonVO.java
+++ HJPA/src/main/java/cn/tannn/hjpa/vo/CommonVO.java
@@ -1,7 +1,6 @@
 package cn.tannn.hjpa.vo;
 
 import cn.jdevelops.entity.basics.vo.SerializableVO;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -20,19 +19,16 @@ public class CommonVO<V> extends SerializableVO<V> {
 	/**
 	 * id
 	 */
-	@ApiModelProperty(value = "id")
 	private Integer id;
 
 	/**
 	 * 表示该字段为创建时间字段，
 	 */
-	@ApiModelProperty(value = "创建时间")
 	private String createTime;
 
 	/**
 	 * 表示该字段为创建人，
 	 */
-	@ApiModelProperty(value = "创建人")
 	private String createUserName;
 
 
--- HJPA/src/main/java/cn/tannn/hjpa/vo/UserVO.java
+++ HJPA/src/main/java/cn/tannn/hjpa/vo/UserVO.java
@@ -1,7 +1,5 @@
 package cn.tannn.hjpa.vo;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,7 +10,6 @@ import lombok.ToString;
  * @author tn
  * @date 2021-06-08 15:26
  */
-@ApiModel("用户基础信息")
 @Getter
 @Setter
 @ToString
@@ -21,42 +18,35 @@ public class UserVO extends CommonVO<UserVO> {
 	/**
 	 * 用户编号
 	 */
-	@ApiModelProperty(value = "用户编号")
 	private String userNo;
 
 	/**
 	 * 姓名
 	 */
-	@ApiModelProperty(value = " 姓名 ")
 	private String name;
 
 	/**
 	 * 登录名称
 	 */
-	@ApiModelProperty(value = "登录名称")
 	private String loginName;
 
 	/**
 	 * 地址
 	 */
-	@ApiModelProperty(value = " 地址 ")
 	private String address;
 
 	/**
 	 * 用户类型
 	 */
-	@ApiModelProperty(value = "  用户类型")
 	private Integer userType;
 
 	/**
 	 * 用户头像
 	 */
-	@ApiModelProperty(value = " 用户头像 ")
 	private String userIcon;
 
 	/**
 	 * 手机号/联系电话
 	 */
-	@ApiModelProperty(value = " 手机号/联系电话 ")
 	private String phone;
 }
