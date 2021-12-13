02f6eff1508c024bb8ad85028729c00453e3e379
tan <1445763190@qq.com>
2021-12-13 18:41:11 +0800

    [示例代码] apisign 自定义盐

--- ApiSign/src/main/java/cn/tannn/apisign/utils/SignUtil.java
+++ ApiSign/src/main/java/cn/tannn/apisign/utils/SignUtil.java
@@ -1,7 +1,8 @@
 package cn.tannn.apisign.utils;
 
-import cn.jdevelops.encryption.constant.SignConstant;
+import cn.jdevelops.apisign.bean.ApiSignBean;
 import cn.jdevelops.encryption.core.SignMD5Util;
+import cn.jdevelops.spring.core.context.ContextUtil;
 import cn.tannn.apisign.entity.UserEntity;
 import com.alibaba.fastjson.JSON;
 
@@ -10,6 +11,7 @@ import java.util.LinkedHashMap;
 
 /**
  * 验证工具
+ *
  * @author tn
  * @version 1
  * @date 2020/12/22 10:03
@@ -17,15 +19,17 @@ import java.util.LinkedHashMap;
 public class SignUtil extends cn.jdevelops.apisign.util.SignUtil {
 
 
-    public static String getMd5SignByJson(UserEntity userEntity){
-        String encrypt1 = SignMD5Util.encrypt(JSON.toJSONString(userEntity),true);
-        return SignMD5Util.encrypt(encrypt1+ SignConstant.MD5_PRIVATE_KEY, true);
+    public static String getMd5SignByJson(UserEntity userEntity) {
+        ApiSignBean apiSignBean = ContextUtil.getBean(ApiSignBean.class);
+        String encrypt1 = SignMD5Util.encrypt(JSON.toJSONString(userEntity), true);
+        return SignMD5Util.encrypt(encrypt1 + apiSignBean.getSalt(), true);
     }
 
-    public static String getMd5SignByBean2LinkedHashMap2Str(UserEntity userEntity){
+    public static String getMd5SignByBean2LinkedHashMap2Str(UserEntity userEntity) {
+        ApiSignBean apiSignBean = ContextUtil.getBean(ApiSignBean.class);
         LinkedHashMap<String, Object> stringObjectLinkedHashMap = TempUtil.beanToLinkedHashMap(userEntity);
-        String encrypt1 = SignMD5Util.encrypt(map2Str(stringObjectLinkedHashMap),true);
-        return SignMD5Util.encrypt(encrypt1+ SignConstant.MD5_PRIVATE_KEY, true);
+        String encrypt1 = SignMD5Util.encrypt(map2Str(stringObjectLinkedHashMap), true);
+        return SignMD5Util.encrypt(encrypt1 + apiSignBean.getSalt(), true);
     }
 
 }
