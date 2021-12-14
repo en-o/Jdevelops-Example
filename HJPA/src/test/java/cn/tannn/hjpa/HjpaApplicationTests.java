13993c541cbe3831ec4f7c2a5d7689dde3661f34
tan <1445763190@qq.com>
2021-12-14 18:35:47 +0800

    [示例代码] jps 高阶查询

--- HJPA/src/test/java/cn/tannn/hjpa/HjpaApplicationTests.java
+++ HJPA/src/test/java/cn/tannn/hjpa/HjpaApplicationTests.java
@@ -1,16 +1,14 @@
 package cn.tannn.hjpa;
 
-import cn.jdevelops.jap.core.util.CommUtils;
 import cn.jdevelops.jap.core.util.JPAUtilExpandCriteria;
+import cn.jdevelops.jap.core.util.SpecificationUtil;
 import cn.jdevelops.jap.core.util.criteria.Restrictions;
 import cn.tannn.hjpa.entity.User;
 import cn.tannn.hjpa.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -37,5 +35,90 @@ class HjpaApplicationTests {
 //        jpaSelect.and(where1);
 //        userService.getJpaBasicsDao().findAll(jpaSelect);
     }
+
+
+    @Test
+    void testSpecificationUtil() {
+        /**
+         *
+         WHERE
+         ( address LIKE '%重庆%' )
+         AND (
+         NAME = '用户'
+         OR login_pwd = '123'
+         OR phone = '123'
+         OR user_no = '123')
+         */
+//        address().and(
+//        name().or(loginPwd()).or(phone()).or(userNo())
+//        )
+        SpecificationUtil<User> instance = SpecificationUtil.getInstance();
+        Specification<User> and = instance.like("address", "重庆", true)
+                .and(instance.eq("name", "用户", true)
+                        .or(instance.eq("loginPwd", "123", true))
+                        .or(instance.eq("phone", "123", true))
+                        .or(instance.eq("userNo", "123", true))
+                );
+        userService.getJpaBasicsDao().findAll(and).forEach(System.out::println);
+    }
+
+
+
+    @Test
+    void testAndOr1() {
+        /**
+         * WHERE
+         * 	( address LIKE '重庆' )
+         * 	AND (
+         * 		NAME LIKE '%用户%'
+         * 		OR login_pwd LIKE '123'
+         * 	    OR phone LIKE '123'
+         * 	    OR phone LIKE '123'
+         * 	    )
+         */
+        userService.getJpaBasicsDao().findAll(
+             address().and(name().or(loginPwd()).or(phone()).or(userNo()) )
+        ).forEach(System.out::println);
+    }
+
+
+    @Test
+    void testAndOr2() {
+        userService.getJpaBasicsDao().findAll(
+                address().and(
+                        loginPwd().or(phone()).or(name())
+                ).or(
+                        loginPwd().and(
+                                phone().or(name())
+                        )
+                )
+        ).forEach(System.out::println);
+    }
+    @Test
+    void test3() {
+        Specification<User>  test = (root, criteriaQuery, criteriaBuilder) -> criteriaQuery.getRestriction();;
+        userService.getJpaBasicsDao().findAll(test).forEach(System.out::println);
+    }
+
+    public static Specification<User> name() {
+        return (root, query, builder) -> builder.like(root.get("name"), "%用户%");
+    }
+
+    public static Specification<User> loginPwd() {
+        return (root, query, builder) -> builder.like(root.get("loginPwd"), "123");
+    }
+
+    public static Specification<User> address() {
+        return (root, query, builder) -> builder.like(root.get("address"), "重庆");
+    }
+
+    public static Specification<User> phone() {
+        return (root, query, builder) -> builder.like(root.get("phone"), "123");
+    }
+
+    public static Specification<User> userNo() {
+        return (root, query, builder) -> builder.like(root.get("phone"), "123");
+    }
+
 
 }
