6e8f89d01340143b4b2cead436b89d428607a4c6
tan <1445763190@qq.com>
2022-04-03 00:48:50 +0800

    [file] file

---
+++ file/src/main/java/cn/jdevelops/file/controller/FileController.java
@@ -0,0 +1,79 @@
+package cn.jdevelops.file.controller;
+
+import cn.jdevelops.exception.exception.BusinessException;
+import cn.jdevelops.file.OssOperateAPI;
+import cn.jdevelops.file.bean.*;
+import cn.jdevelops.result.result.ResultVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * 文件上传下载
+ *
+ * @author lxw
+ * @version V1.0
+ * @date 2021/11/10
+ */
+@RestController
+@Api(tags = "文件操作", value = "文件管理")
+public class FileController {
+
+
+	@Autowired
+	private OssOperateAPI fileOperation;
+
+
+
+	@ApiOperation(value = "文件上传", notes = "文件管理")
+	@PostMapping(value = "upload")
+	public ResultVO<FilePathResult> upload(@Valid UploadDTO uploadDTO) {
+		try {
+			FilePathResult filePathResult = fileOperation.uploadFile(uploadDTO);
+			return ResultVO.successForData(filePathResult);
+		} catch (Exception e) {
+			throw new BusinessException("文件上传失败！", e);
+		}
+	}
+
+
+
+	@GetMapping("/download")
+	@ApiOperation(value = "文件下载", notes = "文件管理")
+	public void download(HttpServletResponse response, @Valid DownloadDTO dto) {
+		try {
+			fileOperation.downloadFile(response, dto);
+		} catch (Exception e) {
+			throw new BusinessException("文件下载失败！");
+		}
+	}
+
+	@GetMapping("/getExpiryObjectUrl")
+	@ApiOperation(value = "获取有效期访问地址", notes = "文件管理")
+	public ResultVO<String> getExpiryObjectUrl(@Valid ExpireDateDTO dto) {
+		try {
+			String url = fileOperation.expireDateUrl(dto);
+			return ResultVO.successForData(url);
+		} catch (Exception e) {
+			throw new BusinessException("获取有效期访问地址失败！");
+		}
+	}
+
+
+
+	@DeleteMapping("/removeObjects")
+	@ApiOperation(value = "删除", notes = "文件管理")
+	public ResultVO<List<String>> removeObjects(@RequestBody @Valid RemoveFileDTO dto) {
+		try {
+			fileOperation.removeFiles(dto);
+			return ResultVO.success();
+		} catch (Exception e) {
+			throw new BusinessException("删除失败！",e);
+		}
+	}
+}
--- file/src/test/java/cn/jdevelops/file/FileApplicationTests.java
+++ file/src/test/java/cn/jdevelops/file/FileApplicationTests.java
@@ -1,42 +1,46 @@
-package cn.jdevelops.file;
-
-import cn.jdevelops.file.bean.ExpireDateDTO;
-import cn.jdevelops.file.bean.UploadDTO;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-
-@SpringBootTest
-class FileApplicationTests {
-
-    @Autowired
-    private  OssOperateAPI fileOperation;
-
-
-
-    @Test
-    void contextLoads() {
-
-//        File file = new File("D:\\ImageFiles\\credits\\image\\shop\\1618368816089.PNG");
-//        DiskFileItemFactory factory = new DiskFileItemFactory(16, null);
-//        FileItem item = factory.createItem(file.getName(), "text/plain", true, file.getName());
-//        MultipartFile multipartFile = new CommonsMultipartFile(item);
-
-        UploadDTO uploadDTO = new UploadDTO();
-        uploadDTO.setBucket("test1111");
-//        uploadDTO.setFile("multipartFile");
-        fileOperation.uploadFile(uploadDTO);
-    }
-
-
-    @Test
-    void expireDateUrl() {
-        ExpireDateDTO expireDateDTO = new ExpireDateDTO();
-        expireDateDTO.setBucket("test1111");
-        expireDateDTO.setChildFolder_FreshName("123/20211215160853365.jpg");
-        expireDateDTO.setExpires(100);
-        System.out.println(fileOperation.expireDateUrl(expireDateDTO));
-    }
-
-}
+//package cn.jdevelops.file;
+//
+//import cn.jdevelops.file.bean.ExpireDateDTO;
+//import cn.jdevelops.file.bean.UploadDTO;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.web.multipart.MultipartFile;
+//import org.springframework.web.multipart.commons.CommonsMultipartFile;
+//
+//
+//@SpringBootTest
+//class FileApplicationTests {
+//
+//    @Autowired
+//    private  OssOperateAPI fileOperation;
+//
+//
+//
+//    @Test
+//    void contextLoads() {
+//
+////        File file = new File("D:\\ImageFiles\\credits\\image\\shop\\1618368816089.PNG");
+////        DiskFileItemFactory factory = new DiskFileItemFactory(16, null);
+////        FileItem item = factory.createItem(file.getName(), "text/plain", true, file.getName());
+////        MultipartFile multipartFile = new CommonsMultipartFile(item);
+//
+//        MultipartFile commonsMultipartFile = new CommonsMultipartFile(Util.createFileItem("D:\\ImageFiles\\credits\\image\\shop\\1618368816089.PNG"));
+//        UploadDTO uploadDTO = new UploadDTO();
+//        uploadDTO.setBucket("test1111");
+//        uploadDTO.setChildFolder("");
+//        uploadDTO.setFile(commonsMultipartFile);
+//        fileOperation.uploadFile(uploadDTO);
+//    }
+//
+//
+//    @Test
+//    void expireDateUrl() {
+//        ExpireDateDTO expireDateDTO = new ExpireDateDTO();
+//        expireDateDTO.setBucket("test1111");
+//        expireDateDTO.setChildFolder_FreshName("123/20211215160853365.jpg");
+//        expireDateDTO.setExpires(100);
+//        System.out.println(fileOperation.expireDateUrl(expireDateDTO));
+//    }
+//
+//}
