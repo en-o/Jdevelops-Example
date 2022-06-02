7d5d3e671f3d394615b344ba6b3f26ed643f78b9
tan <1445763190@qq.com>
2022-06-02 16:24:44 +0800

    测试jpa更新方法

--- TestWebJPA/src/test/java/cn/tannn/testwebjpa/TestWebJpaApplicationTests.java
+++ TestWebJPA/src/test/java/cn/tannn/testwebjpa/TestWebJpaApplicationTests.java
@@ -1,13 +1,38 @@
 package cn.tannn.testwebjpa;
 
+import cn.tannn.testwebjpa.dao.UserDao;
+import cn.tannn.testwebjpa.entity.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestWebJpaApplicationTests {
 
+    @Autowired
+    private UserDao userDao;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+
+    }
+
+
+    @Test
+    void testUpdate() throws Exception {
+        User user = new User();
+        user.setUserNo("123");
+        user.setName("根据指定key更新");
+        userDao.updateEntity(user,"userNo");
+    }
+
+
+    @Test
+    void testUpdate2() throws Exception {
+        User user = new User();
+        user.setId(1);
+        user.setName("根据ID更新");
+        userDao.updateEntity(user);
     }
 
 }
