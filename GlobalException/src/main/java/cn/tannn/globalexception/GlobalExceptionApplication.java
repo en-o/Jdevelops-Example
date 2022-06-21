b11bf204c05a96a3e85a24622f2ae585dfbef73e
tan <1445763190@qq.com>
2022-06-21 10:16:06 +0800

    测试返回类的链式调用

--- GlobalException/src/main/java/cn/tannn/globalexception/GlobalExceptionApplication.java
+++ GlobalException/src/main/java/cn/tannn/globalexception/GlobalExceptionApplication.java
@@ -2,10 +2,13 @@ package cn.tannn.globalexception;
 
 import cn.jdevelops.exception.exception.BusinessException;
 import cn.jdevelops.result.result.ResultVO;
+import com.google.common.collect.ImmutableMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @author tnnn
@@ -36,5 +39,40 @@ public class GlobalExceptionApplication {
     public ResultVO<Integer> testGlobalTry(){
         throw  new BusinessException("测试全局异常拦截处理");
     }
+
+
+
+    /**
+     * 测试 ResultVO
+     * @return ResultVO
+     */
+    @GetMapping("/testResult0")
+    public ResultVO<Integer> testResult0(){
+        return ResultVO.successForData(1);
+    }
+
+
+
+    /**
+     * 测试 ResultVO
+     * @return ResultVO
+     */
+    @GetMapping("/testResult1")
+    public ResultVO<Integer> testResult1(){
+        return ResultVO.<Integer>builder().code(100).message("测试").data(1).build();
+    }
+
+
+    /**
+     * 测试 ResultVO
+     * @return ResultVO
+     */
+    @GetMapping("/testResult2")
+    public ResultVO<Map<String,String>> testResult2(){
+        return ResultVO.<Map<String,String>>builder().code(100).message("测试")
+                .data(ImmutableMap.of("key","value")).build();
+    }
+
+
 
 }
