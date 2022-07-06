d2fdbc869d613eae6ae4175fdc2780e0cf5c3e8c
tan <1445763190@qq.com>
2022-07-06 10:44:17 +0800

    test: updateByBean

---
+++ HJPA/src/test/java/cn/tannn/hjpa/UpdateTest.java
@@ -0,0 +1,43 @@
+package cn.tannn.hjpa;
+
+import cn.jdevelops.jap.core.util.CommUtils;
+import cn.jdevelops.jap.core.util.JPAUtilExpandCriteria;
+import cn.tannn.hjpa.entity.User;
+import cn.tannn.hjpa.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 测试更行
+ *
+ * @author tnnn
+ * @version V1.0
+ * @date 2022-07-06 10:26
+ */
+@SpringBootTest
+//@Transactional(rollbackFor = Exception.class)
+public class UpdateTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void  updateByBean() throws Exception {
+        User user = new User();
+        user.setName("超级管理员3");
+        user.setLoginPwd("user");
+        user.setPhone("15888888888");
+        System.out.println(userService.updateByBean(user, "phone"));
+    }
+
+    @Test
+    public void  updateByBean2() throws Exception {
+        User user = new User();
+        user.setName("超级管理员6");
+        user.setLoginPwd("updateByBean2");
+        user.setPhone("1312");
+        System.out.println(userService.updateByBean(user, User::getPhone));
+    }
+}
