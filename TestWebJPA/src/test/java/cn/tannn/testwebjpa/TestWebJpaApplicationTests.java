b59ad7e563f7b32da997fca64743e7ceb0209f8d
tan <1445763190@qq.com>
2022-07-28 11:36:44 +0800

    测试 SortUtil.sort

--- TestWebJPA/src/test/java/cn/tannn/testwebjpa/TestWebJpaApplicationTests.java
+++ TestWebJPA/src/test/java/cn/tannn/testwebjpa/TestWebJpaApplicationTests.java
@@ -1,10 +1,14 @@
 package cn.tannn.testwebjpa;
 
+import cn.jdevelops.jap.core.util.SortUtil;
 import cn.tannn.testwebjpa.dao.UserDao;
 import cn.tannn.testwebjpa.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 @SpringBootTest
 class TestWebJpaApplicationTests {
@@ -33,6 +37,12 @@ class TestWebJpaApplicationTests {
         user.setId(1);
         user.setName("根据ID更新");
         userDao.updateEntity(user);
+    }
+
+    @Test
+    void testSortUtil(){
+        List<User> all = userDao.findAll(SortUtil.sort(Sort.Direction.DESC, User::getId));
+        all.forEach(it -> System.err.println(it.toString()));
     }
 
 }
