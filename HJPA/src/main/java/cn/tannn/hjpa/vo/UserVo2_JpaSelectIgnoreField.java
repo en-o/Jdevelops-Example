5bb19028995e4ad3d6d0b3b2e9b53724c4f903fa
tan <1445763190@qq.com>
2022-10-09 10:23:16 +0800

    测试 JpaSelectIgnoreField

---
+++ HJPA/src/main/java/cn/tannn/hjpa/vo/UserVo2_JpaSelectIgnoreField.java
@@ -0,0 +1,41 @@
+package cn.tannn.hjpa.vo;
+
+import cn.jdevelops.jap.annotation.JpaSelectIgnoreField;
+import cn.jdevelops.jap.annotation.JpaSelectOperator;
+import cn.jdevelops.jap.enums.SQLConnect;
+import cn.jdevelops.jap.enums.SQLOperator;
+import lombok.Data;
+
+@Data
+public class UserVo2_JpaSelectIgnoreField {
+    /**
+     * 用户编号
+     */
+    @JpaSelectOperator(operator = SQLOperator.LIKE, connect = SQLConnect.OR)
+    private String userNo;
+
+    /**
+     * 姓名
+     */
+    @JpaSelectOperator(operator = SQLOperator.LIKE, connect = SQLConnect.OR, ignoreNull = false)
+    private String name;
+
+    /**
+     * 地址
+     */
+    @JpaSelectOperator(operator = SQLOperator.LIKE, connect = SQLConnect.OR)
+    private String address;
+
+    /**
+     * 登录名称
+     */
+    @JpaSelectOperator(operator = SQLOperator.EQ, connect = SQLConnect.AND)
+    private String loginName;
+
+    /**
+     * 测试忽略 @JpaSelectIgnoreField
+     */
+    @JpaSelectIgnoreField
+    private String ignoreMe;
+
+}
--- HJPA/src/test/java/cn/tannn/hjpa/CommUtilsTest.java
+++ HJPA/src/test/java/cn/tannn/hjpa/CommUtilsTest.java
@@ -3,15 +3,20 @@ package cn.tannn.hjpa;
 import cn.jdevelops.jap.annotation.JpaSelectOperator;
 import cn.jdevelops.jap.core.util.CommUtils;
 import cn.jdevelops.jap.core.util.JPAUtilExpandCriteria;
+import cn.jdevelops.jap.core.util.JpaUtils;
 import cn.jdevelops.jap.enums.SQLConnect;
 import cn.jdevelops.jap.enums.SQLOperator;
 import cn.tannn.hjpa.entity.User;
 import cn.tannn.hjpa.service.UserService;
 import cn.tannn.hjpa.vo.UserVo2;
+import cn.tannn.hjpa.vo.UserVo2_JpaSelectIgnoreField;
+import cn.tannn.hjpa.vo.UserVo2_NoJpaSelectIgnoreField;
 import lombok.Data;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.Column;
@@ -47,6 +52,35 @@ public class CommUtilsTest {
         user.setLoginName("admin");
         user.setAddress("重");
         JPAUtilExpandCriteria<User> selectBean = CommUtils.getSelectBean2(user);
+        userService.getJpaBasicsDao().findAll(selectBean).forEach(System.out::println);
+    }
+
+
+    /**
+     * 测试 @JpaSelectIgnoreField 不加
+     */
+    @Test
+    public void  getSelectBean2JpaSelectIgnoreField_1(){
+        UserVo2_NoJpaSelectIgnoreField user = new UserVo2_NoJpaSelectIgnoreField();
+        user.setLoginName("admin");
+        user.setAddress("重");
+        user.setIgnoreMe("123");
+        JPAUtilExpandCriteria<User> selectBean = JpaUtils.getSelectBean2(user);
+        Assert.assertThrows( InvalidDataAccessApiUsageException.class, () -> userService.getJpaBasicsDao().findAll(selectBean));
+    }
+
+
+    /**
+     * 测试 @JpaSelectIgnoreField
+     */
+    @Test
+    public void  getSelectBean2JpaSelectIgnoreField_2(){
+        UserVo2_JpaSelectIgnoreField user = new UserVo2_JpaSelectIgnoreField();
+//        user.setName("");
+        user.setIgnoreMe("123");
+        user.setLoginName("admin");
+        user.setAddress("重");
+        JPAUtilExpandCriteria<User> selectBean = JpaUtils.getSelectBean2(user);
         userService.getJpaBasicsDao().findAll(selectBean).forEach(System.out::println);
     }
 
