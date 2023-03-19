3a452e65d3c68f0493d8db5679febbd3c24b9c96
tan <13320385140@163.com>
2023-03-19 20:41:08 +0800

    测试jdevelops-sboot-jpa-demo

--- jdevelops-sboot-jpa-demo/src/main/java/cn/tannn/jdevelopssbootjpademo/dto/UserFindDTO.java
+++ jdevelops-sboot-jpa-demo/src/main/java/cn/tannn/jdevelopssbootjpademo/dto/UserFindDTO.java
@@ -1,5 +1,8 @@
 package cn.tannn.jdevelopssbootjpademo.dto;
 
+import cn.jdevelops.data.jap.annotation.JpaSelectOperator;
+import cn.jdevelops.data.jap.enums.SQLConnect;
+import cn.jdevelops.data.jap.enums.SQLOperator;
 import cn.jdevelops.result.bean.SerializableBean;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,24 +21,29 @@ public class UserFindDTO extends SerializableBean<UserFindDTO> {
 	/**
 	 * 用户编号
 	 */
+	@JpaSelectOperator(operator = SQLOperator.LIKE ,connect = SQLConnect.AND)
 	private String userNo;
 	/**
 	 * 姓名
 	 */
+	@JpaSelectOperator(operator = SQLOperator.LIKE ,connect = SQLConnect.AND)
 	private String name;
 	/**
 	 * 地址
 	 */
+	@JpaSelectOperator(operator = SQLOperator.LIKE ,connect = SQLConnect.AND)
 	private String address;
 
 	/**
 	 * 用户头像
 	 */
+	@JpaSelectOperator(operator = SQLOperator.LIKE ,connect = SQLConnect.AND)
 	private String userIcon;
 
 	/**
 	 * 手机号/联系电话
 	 */
+	@JpaSelectOperator(operator = SQLOperator.EQ ,connect = SQLConnect.AND)
 	private String phone;
 
 }
