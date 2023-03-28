4ba580b0ab1e1392a75b76005c6778fd423071e5
tan <13320385140@163.com>
2023-03-28 10:05:12 +0800

    重新命名包

--- jdevelops-api-log-demo/src/main/java/cn/tan/jdevelops/api/log/JdevelopsApiLogApplication.java
+++ jdevelops-api-log-demo/src/main/java/cn/tan/jdevelops/api/log/JdevelopsApiLogApplication.java
@@ -3,6 +3,9 @@ package cn.tan.jdevelops.api.log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author tan
+ */
 @SpringBootApplication
 public class JdevelopsApiLogApplication {
 
