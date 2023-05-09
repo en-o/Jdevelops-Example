d0cf65d49b724e0759bc17bd4c764d41ca942afb
tan <13320385140@163.com>
2023-05-09 12:12:40 +0800

    fix： 自定建库

--- jdevelops-data-auto-schema-demo/src/main/java/com/example/jdevelopsdataautoschemademo/JdevelopsDataAutoSchemaDemoApplication.java
+++ jdevelops-data-auto-schema-demo/src/main/java/com/example/jdevelopsdataautoschemademo/JdevelopsDataAutoSchemaDemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.jdevelopsdataautoschemademo;
 
+import cn.jdevelops.schema.scan.EnableAutoSchema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
