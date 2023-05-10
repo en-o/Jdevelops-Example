843dd27db8af2801d63f644795a43fe3a8e7474b
tan <13320385140@163.com>
2023-05-10 14:04:18 +0800

    日志

--- jdevelops-api-log-demo/src/main/java/cn/tan/jdevelops/api/log/save/config/ApiLogSaveImpl.java
+++ jdevelops-api-log-demo/src/main/java/cn/tan/jdevelops/api/log/save/config/ApiLogSaveImpl.java
@@ -1,23 +1,23 @@
-//package cn.tan.jdevelops.api.log.save.config;
-//
-//import cn.jdevelops.aop.api.log.bean.ApiMonitoring;
-//import cn.jdevelops.aop.api.log.server.ApiLogSave;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Component;
-//import org.springframework.stereotype.Service;
-//
-///**
-// * 重新实现ApiLog日志保存方法
-// *
-// * @author tn
-// * @version 1
-// * @date 2021-12-16 12:34
-// */
+package cn.tan.jdevelops.api.log.save.config;
+
+import cn.jdevelops.aop.api.log.bean.ApiMonitoring;
+import cn.jdevelops.aop.api.log.server.ApiLogSave;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+/**
+ * 重新实现ApiLog日志保存方法
+ *
+ * @author tn
+ * @version 1
+ * @date 2021-12-16 12:34
+ */
 //@Service
-//@Slf4j
-//public class ApiLogSaveImpl implements ApiLogSave {
-//    @Override
-//    public void saveLog(ApiMonitoring apilog) {
-//       log.info("你可以做入库操作同时也可做数据库打印操作："+apilog.toString());
-//    }
-//}
+@Slf4j
+public class ApiLogSaveImpl implements ApiLogSave {
+    @Override
+    public void saveLog(ApiMonitoring apilog) {
+       log.info("你可以做入库操作同时也可做数据库打印操作："+apilog.toString());
+    }
+}
