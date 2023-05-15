36a8bb122eef1f95c9c662dfaa905c383e39703e
tan <13320385140@163.com>
2023-05-15 16:18:10 +0800

    测试swagger

---
+++ file/src/main/java/cn/jdevelops/file/controller/TestDTO.java
@@ -0,0 +1,35 @@
+package cn.jdevelops.file.controller;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * @author tn
+ * @version 1
+ * @date 2021-12-10 22:56
+ */
+@Schema(description = "测试dto")
+@Getter
+@Setter
+@ToString
+public class TestDTO implements Serializable {
+
+    /**
+     * id
+     */
+    @Schema(description = "id")
+    private Integer id;
+
+    /**
+     * id
+     */
+    @Schema(description = "姓名")
+    private Integer name;
+
+
+}
+
