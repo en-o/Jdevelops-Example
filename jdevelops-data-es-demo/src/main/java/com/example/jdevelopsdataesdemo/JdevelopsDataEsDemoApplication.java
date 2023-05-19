d3028205a089c9f07793a701d12cbe7a772755ed
tan <13320385140@163.com>
2023-05-19 17:01:00 +0800

    es

---
+++ jdevelops-data-es-demo/src/main/java/com/example/jdevelopsdataesdemo/JdevelopsDataEsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdevelopsdataesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdevelopsDataEsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdevelopsDataEsDemoApplication.class, args);
+    }
+
+}
---
+++ jdevelops-data-es-demo/src/test/java/com/example/jdevelopsdataesdemo/JdevelopsDataEsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdevelopsdataesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdevelopsDataEsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
