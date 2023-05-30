b034fd049701059e172910617e20fa317fd81e2f
tan <13320385140@163.com>
2023-05-30 09:27:27 +0800

    测试AuditorNameService

--- jdevelops-sboot-jpa-demo/src/main/java/cn/tannn/jdevelopssbootjpademo/JdevelopsSbootJpaDemoApplication.java
+++ jdevelops-sboot-jpa-demo/src/main/java/cn/tannn/jdevelopssbootjpademo/JdevelopsSbootJpaDemoApplication.java
@@ -2,8 +2,10 @@ package cn.tannn.jdevelopssbootjpademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JdevelopsSbootJpaDemoApplication {
 
     public static void main(String[] args) {
--- jdevelops-sboot-jpa-demo/src/test/java/cn/tannn/jdevelopssbootjpademo/JdevelopsSbootJpaDemoApplicationTests.java
+++ jdevelops-sboot-jpa-demo/src/test/java/cn/tannn/jdevelopssbootjpademo/JdevelopsSbootJpaDemoApplicationTests.java
@@ -1,13 +1,39 @@
 package cn.tannn.jdevelopssbootjpademo;
 
+import cn.jdevelops.sboot.jpa.core.auditor.AuditorNameService;
+import cn.tannn.jdevelopssbootjpademo.entity.User;
+import cn.tannn.jdevelopssbootjpademo.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JdevelopsSbootJpaDemoApplicationTests {
 
+    @Autowired
+    private UserService userService;
+
+
     @Test
     void contextLoads() {
+    }
+
+
+    /**
+     * 测试 AuditorNameService
+     */
+    @Test
+    void auditorNameServiceTest() {
+        User user = new User();
+        user.setName("test");
+        user.setPhone("123");
+        user.setUserIcon("");
+        user.setUserNo("test");
+        user.setLoginName("test");
+        user.setLoginPwd("123");
+        user.setAddress("123");
+        userService.saveByBean(user);
+
     }
 
 }
