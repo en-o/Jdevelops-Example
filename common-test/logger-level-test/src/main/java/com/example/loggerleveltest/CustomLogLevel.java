d553625524f03e53b2313bcc74af1605c7404974
tan <13320385140@163.com>
2023-06-04 02:12:29 +0800

    LoggerLevelTestApplicationTests

---
+++ common-test/logger-level-test/src/main/java/com/example/loggerleveltest/CustomLogLevel.java
@@ -0,0 +1,15 @@
+//package com.example.loggerleveltest;
+//
+//import java.util.logging.Level;
+//
+//public class CustomLogLevel extends Level {
+//
+//    private static final long serialVersionUID = 1L;
+//
+//    public static final Level APILOG = new CustomLogLevel("APILOG", 850);
+//
+//    protected CustomLogLevel(String name, int value) {
+//        super(name, value);
+//    }
+//}
+//
