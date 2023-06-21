40a034da72b779383bd9edce3ab77c2a8be6d9b0
en-o <13320385140@163.com>
2023-06-21 14:47:44 +0800

    模块修改测试

--- jdevelops-aop-exception-demo/src/main/java/com/example/jdevelopsaopexceptiondemo/result/ReplaceResultVO.java
+++ jdevelops-aop-exception-demo/src/main/java/com/example/jdevelopsaopexceptiondemo/result/ReplaceResultVO.java
@@ -1,5 +1,11 @@
 package com.example.jdevelopsaopexceptiondemo.result;
 
+
+import cn.jdevelops.api.result.emums.ExceptionCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.io.Serializable;
 
 /**
@@ -9,7 +15,9 @@ import java.io.Serializable;
  * @version 1
  * @date 2020/6/8 17:28
  */
-
+@Getter
+@Setter
+@ToString
 public class ReplaceResultVO<T> implements Serializable {
 
     private static final long serialVersionUID = -7719394736046024902L;
@@ -31,58 +39,62 @@ public class ReplaceResultVO<T> implements Serializable {
 
 
 
-    public static <T> ReplaceResultVO<T> success(int code, String message) {
+    public static <T> ReplaceResultVO<T> success() {
+        ReplaceResultVO<T> resultVO = new ReplaceResultVO<>();
+        resultVO.setZhuangTaiMa(200);
+        resultVO.setXiaoXi("success");
+        return resultVO;
+    }
+
+
+
+    public static <T> ReplaceResultVO<T> fail() {
+        ReplaceResultVO<T> resultVO = new ReplaceResultVO<>();
+        resultVO.setZhuangTaiMa(500);
+        resultVO.setXiaoXi("fail");
+        return resultVO;
+    }
+
+    public static <T> ReplaceResultVO<T> fail(String message) {
+        ReplaceResultVO<T> resultVO = new ReplaceResultVO<>();
+        resultVO.setZhuangTaiMa(500);
+        resultVO.setXiaoXi(message);
+        return resultVO;
+    }
+
+    public static <T> ReplaceResultVO success( T data) {
+        ReplaceResultVO<T> resultVO = new ReplaceResultVO<>();
+        resultVO.setZhuangTaiMa(200);
+        resultVO.setSJu(data);
+        return resultVO;
+    }
+    public static <T> ReplaceResultVO result(int code, String message) {
         ReplaceResultVO<T> resultVO = new ReplaceResultVO<>();
         resultVO.setZhuangTaiMa(code);
         resultVO.setXiaoXi(message);
         return resultVO;
     }
-
-    public static <T> ReplaceResultVO<T> fail(int code, String message, T data) {
+    public static <T> ReplaceResultVO result(int code, String message,T data) {
         ReplaceResultVO<T> resultVO = new ReplaceResultVO<>();
         resultVO.setZhuangTaiMa(code);
         resultVO.setXiaoXi(message);
-        resultVO.setsJu(data);
+        resultVO.setSJu(data);
         return resultVO;
     }
 
-    public static <T> ReplaceResultVO<T> fail(int code, String message) {
+    public static <T> ReplaceResultVO result(ExceptionCode result,T data) {
         ReplaceResultVO<T> resultVO = new ReplaceResultVO<>();
-        resultVO.setZhuangTaiMa(code);
-        resultVO.setXiaoXi(message);
+        resultVO.setZhuangTaiMa(result.getCode());
+        resultVO.setXiaoXi(result.getMessage());
+        resultVO.setSJu(data);
         return resultVO;
     }
 
-    @Override
-    public String toString() {
-        return "ReplaceResultVO{" +
-                "zhuangTaiMa=" + zhuangTaiMa +
-                ", xiaoXi='" + xiaoXi + '\'' +
-                ", sJu=" + sJu +
-                '}';
-    }
 
-    public Integer getZhuangTaiMa() {
-        return zhuangTaiMa;
-    }
-
-    public void setZhuangTaiMa(Integer zhuangTaiMa) {
-        this.zhuangTaiMa = zhuangTaiMa;
-    }
-
-    public String getXiaoXi() {
-        return xiaoXi;
-    }
-
-    public void setXiaoXi(String xiaoXi) {
-        this.xiaoXi = xiaoXi;
-    }
-
-    public T getsJu() {
-        return sJu;
-    }
-
-    public void setsJu(T sJu) {
-        this.sJu = sJu;
+    public static <T> ReplaceResultVO result(ExceptionCode result) {
+        ReplaceResultVO<T> resultVO = new ReplaceResultVO<>();
+        resultVO.setZhuangTaiMa(result.getCode());
+        resultVO.setXiaoXi(result.getMessage());
+        return resultVO;
     }
 }
