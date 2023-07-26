4ab780da9d8635b9dd88fa61ee246d8432e7ecf8
en-o <13320385140@163.com>
2023-07-26 12:53:12 +0800

    test

--- jdevelops-sboot-authentication-sas/resource-server/src/main/java/cn/tan/authentication/sas/resource/web/MessagesController.java
+++ jdevelops-sboot-authentication-sas/resource-server/src/main/java/cn/tan/authentication/sas/resource/web/MessagesController.java
@@ -1,29 +1,32 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.tan.authentication.sas.resource.web;
 
-import cn.jdevelops.api.result.response.ResultVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Joe Grandja
+ * @since 0.0.1
+ */
 @RestController
 public class MessagesController {
 
-	@GetMapping("/resource1")
-	public ResultVO getResource1(){
-		return ResultVO.success("服务A -> 资源1 -> 读权限");
-	}
-
-	@GetMapping("/resource2")
-	public ResultVO getResource2(){
-		return ResultVO.success("服务A -> 资源2 -> 写权限");
-	}
-
-	@GetMapping("/resource3")
-	public ResultVO resource3(){
-		return ResultVO.success("服务A -> 资源3 -> profile 权限");
-	}
-
-	@GetMapping("/api/publicResource")
-	public ResultVO publicResource() {
-		return ResultVO.success("服务A -> 公共资源");
+	@GetMapping("/messages")
+	public String[] getMessages() {
+		return new String[] {"Message 1", "Message 2", "Message 3"};
 	}
 }
