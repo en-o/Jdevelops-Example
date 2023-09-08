2bc462dbcde7e5f0fb2b489320718af8595536c2
en-o <13320385140@163.com>
2023-09-08 14:09:20 +0800

    jwt test

---
+++ jdevelops-sboot-authentication-jredis-demo/src/main/java/com/example/jdevelopssbootauthenticationjredisdemo/controller/LoginController.java
@@ -0,0 +1,72 @@
+package com.example.jdevelopssbootauthenticationjredisdemo.controller;
+
+import cn.jdevelops.api.result.response.ResultVO;
+import cn.jdevelops.sboot.authentication.jredis.entity.RedisAccount;
+import cn.jdevelops.sboot.authentication.jredis.entity.sign.RedisSignEntity;
+import cn.jdevelops.sboot.authentication.jredis.service.RedisLoginService;
+import cn.jdevelops.sboot.authentication.jwt.annotation.ApiMapping;
+import com.example.jdevelopssbootauthenticationjredisdemo.bean.TestBean;
+import com.example.jdevelopssbootauthenticationjredisdemo.bean.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * 登录突出
+ *
+ * @author <a href="https://tannn.cn/">tan</a>
+ * @date 2023/9/8 13:55
+ */
+@RestController
+public class LoginController {
+
+
+    @Autowired
+    private RedisLoginService redisLoginService;
+
+    /**
+     * 退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public ResultVO<String> logout(HttpServletRequest request) {
+        redisLoginService.loginOut(request);
+        return ResultVO.success("成功退出");
+    }
+
+
+    /**
+     * 登录
+     * @param onlyOnline 以前的是否会被挤下线
+     * @return ResultVO
+     */
+    @ApiMapping(value = "/login", checkToken = false, method = RequestMethod.GET)
+    public ResultVO<Object> login(HttpServletRequest request, String username,String password,
+                                  boolean onlyOnline) {
+
+        RedisSignEntity<TestBean> redisSignEntity = new RedisSignEntity<>(username,
+                username,username,username,false,onlyOnline);
+        redisSignEntity.setMap(new TestBean("jwtRedis"));
+        // 此处数据最好用查出来的用,我这里没有查返回所以自己写了
+        RedisAccount<UserInfo> redisAccount = new RedisAccount<>();
+        redisAccount.setDisabledAccount(false);
+        redisAccount.setExcessiveAttempts(false);
+        redisAccount.setUserCode(username);
+        redisAccount.setSalt(username);
+        redisAccount.setPassword(password);
+        UserInfo userInfo = new UserInfo("tan",10);
+//        redisAccount.setUserInfo(null);
+        redisAccount.setUserInfo(userInfo);
+        String sign = redisLoginService.login(redisSignEntity,
+                redisAccount);
+        Map<String, String> responseData = Collections.singletonMap("token", sign);
+        return ResultVO.success(responseData);
+    }
+
+}
