534e2b4f8667f017a99e8155230b64048ca43960
en-o <13320385140@163.com>
2023-09-18 09:10:19 +0800

    fix: 包结构

--- jdevelops-data-auto-schema-demo/src/main/java/com/example/jdevelopsdataautoschemademo/JdevelopsDataAutoSchemaDemoApplication.java
+++ jdevelops-data-auto-schema-demo/src/main/java/com/example/jdevelopsdataautoschemademo/JdevelopsDataAutoSchemaDemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.jdevelopsdataautoschemademo;
 
-import cn.jdevelops.schema.scan.EnableAutoSchema;
+import cn.jdevelops.data.schema.scan.EnableAutoSchema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author tan
+ */
 @SpringBootApplication
-@EnableAutoSchema
+@ EnableAutoSchema
 public class JdevelopsDataAutoSchemaDemoApplication {
 
     public static void main(String[] args) {
