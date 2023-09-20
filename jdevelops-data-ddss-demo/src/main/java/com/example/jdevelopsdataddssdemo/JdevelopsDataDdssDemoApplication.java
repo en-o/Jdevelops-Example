dbd8c354a7be23790e0a900f7cbff8a26d84b553
en-o <13320385140@163.com>
2023-09-20 15:50:09 +0800

    ClassLoader 没有加载成功（Driver does not support get/set network timeout for connections. (null)）

--- jdevelops-data-ddss-demo/src/main/java/com/example/jdevelopsdataddssdemo/JdevelopsDataDdssDemoApplication.java
+++ jdevelops-data-ddss-demo/src/main/java/com/example/jdevelopsdataddssdemo/JdevelopsDataDdssDemoApplication.java
@@ -14,10 +14,10 @@ public class JdevelopsDataDdssDemoApplication {
     public static void main(String[] args) throws MalformedURLException {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(JdevelopsDataDdssDemoApplication.class, args);
         // 需要加载的jar包路径
-        URL url = new URL("file:///G/tool/maven/repo/com/microsoft/sqlserver/sqljdbc4/4.0/sqljdbc4-4.0.jar");
-        ClassLoader sysLoader = ClassLoader.getSystemClassLoader();
-        URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{url}, sysLoader);
-        applicationContext.setClassLoader(urlClassLoader);
+//        URL url = new URL("file:///G/tool/maven/repo/com/microsoft/sqlserver/sqljdbc4/4.0/sqljdbc4-4.0.jar");
+//        ClassLoader sysLoader = ClassLoader.getSystemClassLoader();
+//        URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{url}, sysLoader);
+//        applicationContext.setClassLoader(urlClassLoader);
 
     }
 
