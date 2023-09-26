8297b705cc7900d9b9f45958fbc494ba9d9a00cd
en-o <13320385140@163.com>
2023-09-26 15:15:09 +0800

    test: 动态添加驱动

--- jdevelops-data-ddss-demo/src/main/java/com/example/jdevelopsdataddssdemo/JdevelopsDataDdssDemoApplication.java
+++ jdevelops-data-ddss-demo/src/main/java/com/example/jdevelopsdataddssdemo/JdevelopsDataDdssDemoApplication.java
@@ -1,24 +1,20 @@
 package com.example.jdevelopsdataddssdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
+
 
 @SpringBootApplication
+@Slf4j
 public class JdevelopsDataDdssDemoApplication {
 
-    public static void main(String[] args) throws MalformedURLException {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(JdevelopsDataDdssDemoApplication.class, args);
-        // 需要加载的jar包路径
-//        URL url = new URL("file:///G/tool/maven/repo/com/microsoft/sqlserver/sqljdbc4/4.0/sqljdbc4-4.0.jar");
-//        ClassLoader sysLoader = ClassLoader.getSystemClassLoader();
-//        URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{url}, sysLoader);
-//        applicationContext.setClassLoader(urlClassLoader);
-
+    public static void main(String[] args) {
+        SpringApplication.run(JdevelopsDataDdssDemoApplication.class, args);
     }
+
+
+
 
 }
