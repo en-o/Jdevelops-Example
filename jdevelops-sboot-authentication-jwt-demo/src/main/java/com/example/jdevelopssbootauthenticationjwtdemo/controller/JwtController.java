6e1b5fd00c1af476ccda857e1f969872d496f412
en-o <13320385140@163.com>
2023-12-05 15:33:08 +0800

    测试LoginJwtExtendInfo

--- jdevelops-sboot-authentication-jwt-demo/src/main/java/com/example/jdevelopssbootauthenticationjwtdemo/controller/JwtController.java
+++ jdevelops-sboot-authentication-jwt-demo/src/main/java/com/example/jdevelopssbootauthenticationjwtdemo/controller/JwtController.java
@@ -5,6 +5,7 @@ import cn.jdevelops.sboot.authentication.jwt.annotation.ApiMapping;
 import cn.jdevelops.sboot.authentication.jwt.server.LoginService;
 import cn.jdevelops.sboot.authentication.jwt.util.JwtWebUtil;
 import cn.jdevelops.util.jwt.core.JwtService;
+import cn.jdevelops.util.jwt.entity.LoginJwtExtendInfo;
 import cn.jdevelops.util.jwt.entity.SignEntity;
 import com.example.jdevelopssbootauthenticationjwtdemo.bean.TestBean;
 import com.example.jdevelopssbootauthenticationjwtdemo.result.ReplaceResultVO;
@@ -29,73 +30,84 @@ public class JwtController {
     private LoginService loginService;
 
     @GetMapping("stop")
-    public String stop(){
+    public String stop() {
         return "拦截我";
     }
 
 
     @GetMapping("letGo")
-    public ResultVO<String> letGo(){
+    public ResultVO<String> letGo() {
         return ResultVO.success("通过配置放行我");
     }
 
     @GetMapping("letGo2")
-    public ReplaceResultVO<String> letGo2(){
-        return ReplaceResultVO.success(200,"通过配置放行我");
+    public ReplaceResultVO<String> letGo2() {
+        return ReplaceResultVO.success(200, "通过配置放行我");
     }
 
-    @ApiMapping(value = "/api/letGo",checkToken = false)
-    public String apiLetGo(){
+    @ApiMapping(value = "/api/letGo", checkToken = false)
+    public String apiLetGo() {
         return "利用注解@ApiMapping放行";
     }
 
 
     @GetMapping("/user/login")
-    public String defaultLetGo(){
+    public String defaultLetGo() {
         return "默认放行</user/login>";
     }
 
     @GetMapping("/login")
-    public String token(){
+    public String token() {
         SignEntity<String> signEntity = new SignEntity<>("tan");
         signEntity.setMap("hi");
         return loginService.login(signEntity);
     }
 
-    @ApiMapping(value = "/login2",checkToken = false)
-    public String token2(){
-        SignEntity<TestBean> signEntity = new SignEntity<>("tan",new TestBean("hi"));
+    @ApiMapping(value = "/login2", checkToken = false)
+    public String token2() {
+        SignEntity<TestBean> signEntity = new SignEntity<>("tan", new TestBean("hi"));
         return loginService.login(signEntity);
     }
 
 
-    @ApiMapping(value = "/login3",checkToken = false)
-    public String token3(){
+    @ApiMapping(value = "/login3", checkToken = false)
+    public String token3() {
         Map<String, String> info = new HashMap<>();
-        info.put("nickname","ning");
-        SignEntity<Map<String, String>> signEntity = new SignEntity<>("tan",info);
+        info.put("nickname", "ning");
+        SignEntity<Map<String, String>> signEntity = new SignEntity<>("tan", info);
+        return loginService.login(signEntity);
+    }
+
+    @ApiMapping(value = "/login4", checkToken = false)
+    public String token4() {
+        Map<String, String> info = new HashMap<>();
+        info.put("nickname", "ning");
+        SignEntity<LoginJwtExtendInfo<Map<String, String>>> signEntity = new SignEntity<>("tan",
+                new LoginJwtExtendInfo("tan", "1", "宁", info));
         return loginService.login(signEntity);
     }
 
     @GetMapping("/isLogin")
-    public boolean isLogin(HttpServletRequest request){
+    public boolean isLogin(HttpServletRequest request) {
         return loginService.isLogin(request);
     }
 
     @GetMapping("/parseJwt")
-    public SignEntity<Object> parseJwt(HttpServletRequest request, int type){
-        if(type == 1){
+    public SignEntity<Object> parseJwt(HttpServletRequest request, int type) {
+        if(type == 0){
+            return JwtWebUtil.getTokenBySignEntity(request, LoginJwtExtendInfo.class);
+        }else if (type == 1) {
             return JwtWebUtil.getTokenBySignEntity(request, TestBean.class);
-        }else if(type == 2){
+        } else if (type == 2) {
             return JwtWebUtil.getTokenBySignEntity(request, Map.class);
-        }else {
+        } else {
             return JwtWebUtil.getTokenBySignEntity(request, String.class);
         }
     }
 
 
     @GetMapping("/subject")
-    public String subject(HttpServletRequest request){
+    public String subject(HttpServletRequest request) {
         String token = JwtWebUtil.getToken(request);
         return JwtService.getSubjectExpires(token);
     }
