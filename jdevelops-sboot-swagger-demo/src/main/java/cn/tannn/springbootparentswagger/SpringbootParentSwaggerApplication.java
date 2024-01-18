2a4f03fae4615b36a61bbd862a9d7f2e0b674d7e
en-o <13320385140@163.com>
2024-01-18 16:42:24 +0800

    delete error import

--- jdevelops-sboot-swagger-demo/src/main/java/cn/tannn/springbootparentswagger/SpringbootParentSwaggerApplication.java
+++ jdevelops-sboot-swagger-demo/src/main/java/cn/tannn/springbootparentswagger/SpringbootParentSwaggerApplication.java
@@ -1,15 +1,8 @@
 package cn.tannn.springbootparentswagger;
 
-import cn.jdevelops.sboot.swagger.config.MultipleGroupConfig;
-import cn.jdevelops.sboot.swagger.core.entity.BuildSecuritySchemes;
-import cn.jdevelops.sboot.swagger.domain.SwaggerProperties;
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import static cn.jdevelops.sboot.swagger.core.util.SwaggerUtil.basePackages;
-import static cn.jdevelops.sboot.swagger.core.util.SwaggerUtil.buildSecuritySchemes;
 
 @SpringBootApplication
 public class SpringbootParentSwaggerApplication {
