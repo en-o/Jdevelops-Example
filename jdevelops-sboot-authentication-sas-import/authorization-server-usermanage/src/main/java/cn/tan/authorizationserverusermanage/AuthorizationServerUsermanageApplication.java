2f32c504f73cf0bc1f32dff41eeeb240e0381961
en-o <13320385140@163.com>
2024-01-25 16:56:54 +0800

    测试完成 authentication server

---
+++ jdevelops-sboot-authentication-sas-import/authorization-server-usermanage/src/main/java/cn/tan/authorizationserverusermanage/AuthorizationServerUsermanageApplication.java
@@ -0,0 +1,59 @@
+package cn.tan.authorizationserverusermanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AuthorizationServerUsermanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerUsermanageApplication.class, args);
+	}
+
+
+
+
+	/**
+	 * 登录就行
+	 */
+	@GetMapping("/messages1")
+	public String getMessages1() {
+		return " hello Message 1";
+	}
+
+
+	/**
+	 * PreAuthorize(如果有指定权限, 则返回 true)
+	 */
+	@GetMapping("/messages2")
+	@PreAuthorize("hasAuthority('SCOPE_profile')")
+	public String getMessages2() {
+		return " hello Message 2";
+	}
+
+
+	/**
+	 * PreAuthorize(如果有指定权限, 则返回 true)
+	 */
+	@GetMapping("/messages3")
+	@PreAuthorize("hasAuthority('SCOPE_Message')")
+	public String getMessages3() {
+		return " hello Message 3";
+	}
+
+
+
+	/**
+	 * PreAuthorize(如果有指定权限, 则返回 true)
+	 */
+	@GetMapping("/messages4")
+	@PreAuthorize("hasAuthority('SCOPE_tan')")
+	public String getMessages4() {
+		return " hello Message 4";
+	}
+}
---
+++ jdevelops-sboot-authentication-sas-import/authorization-server-usermanage/src/main/java/cn/tan/authorizationserverusermanage/service/UserInfoService.java
@@ -0,0 +1,14 @@
+package cn.tan.authorizationserverusermanage.service;
+
+import cn.tan.authorizationserverusermanage.dao.UserInfoDao;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author <a href="https://tannn.cn/">tan</a>
+ * @date 2024/1/25 13:34
+ */
+@Service
+public class UserInfoService {
+    private UserInfoDao userInfoDao;
+
+}
