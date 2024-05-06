1cf5a29c95eddb53f129bc0c374cfd7aa9ff3897
tan <13320385140@163.com>
2024-05-06 18:22:23 +0800

    删除多余的

--- jdevelops-config-demo/src/main/java/com/example/jdevelopsconfigdemo/JdevelopsConfigDemoApplication.java
+++ jdevelops-config-demo/src/main/java/com/example/jdevelopsconfigdemo/JdevelopsConfigDemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-//@EntityScan(basePackageClasses = {Configs.class, User.class})
 public class JdevelopsConfigDemoApplication {
 
     public static void main(String[] args) {
