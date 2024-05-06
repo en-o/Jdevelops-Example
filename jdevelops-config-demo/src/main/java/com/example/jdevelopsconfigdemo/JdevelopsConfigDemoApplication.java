f300be8dcfe283d4cab1bcb1211be34149e70727
en-o <13320385140@163.com>
2024-05-06 16:32:25 +0800

    临时的

--- jdevelops-config-demo/src/main/java/com/example/jdevelopsconfigdemo/JdevelopsConfigDemoApplication.java
+++ jdevelops-config-demo/src/main/java/com/example/jdevelopsconfigdemo/JdevelopsConfigDemoApplication.java
@@ -1,17 +1,13 @@
 package com.example.jdevelopsconfigdemo;
 
-import cn.jdevelops.config.standalone.model.Configs;
-import com.example.jdevelopsconfigdemo.test.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
-@EntityScan(basePackageClasses = {Configs.class, User.class})
 public class JdevelopsConfigDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JdevelopsConfigDemoApplication.class, args);
     }
-
 }
