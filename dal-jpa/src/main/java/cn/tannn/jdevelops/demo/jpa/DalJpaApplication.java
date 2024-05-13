85fff09c1a8b856d29e5ceb200c3c2ea34298f74
en-o <13320385140@163.com>
2024-05-13 16:17:28 +0800

    init dal-jpa test

---
+++ dal-jpa/src/main/java/cn/tannn/jdevelops/demo/jpa/DalJpaApplication.java
@@ -0,0 +1,13 @@
+package cn.tannn.jdevelops.demo.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DalJpaApplication.class, args);
+    }
+
+}
---
+++ dal-jpa/src/test/java/cn/tannn/jdevelops/demo/jpa/DalJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tannn.jdevelops.demo.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
