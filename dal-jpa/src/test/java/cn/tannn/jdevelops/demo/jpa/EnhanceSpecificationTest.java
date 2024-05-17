ffef6cfb3ba55bbdb8f791c860af1efa8985f7d6
en-o <13320385140@163.com>
2024-05-17 09:43:32 +0800

    EnhanceSpecificationTest

---
+++ dal-jpa/src/test/java/cn/tannn/jdevelops/demo/jpa/EnhanceSpecificationTest.java
@@ -0,0 +1,19 @@
+package cn.tannn.jdevelops.demo.jpa;
+
+import cn.tannn.jdevelops.demo.jpa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 复杂查询核心方法的使用示例
+ *
+ * @author <a href="https://tannn.cn/">tan</a>
+ * @date 2024/5/17 上午9:39
+ */
+@SpringBootTest
+public class EnhanceSpecificationTest {
+
+    @Autowired
+    private UserService userService;
+
+}
