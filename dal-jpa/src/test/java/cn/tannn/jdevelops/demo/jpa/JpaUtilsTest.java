528a503bf6464dcaa2f47089e8c9267c52a9b8de
en-o <13320385140@163.com>
2024-05-17 10:31:26 +0800

    toSql

--- dal-jpa/src/test/java/cn/tannn/jdevelops/demo/jpa/JpaUtilsTest.java
+++ dal-jpa/src/test/java/cn/tannn/jdevelops/demo/jpa/JpaUtilsTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.EntityManager;
 import javax.persistence.criteria.Predicate;
 
 /**
@@ -57,5 +58,14 @@ public class JpaUtilsTest {
                         "2019-05-15 11:03:31","2090-05-15 11:03:31");
         // from sys_user user0_ where date_format(user0_.create_time, ?) between ? and ?
         userService.getJpaBasicsDao().findAll(between).forEach(System.out::println);
+    }
+
+    @Test
+    void toSql() {
+        EntityManager entityManager = userService.getEntityManager();
+        // ===> sql: select generatedAlias0 from User as generatedAlias0 where generatedAlias0.name=:param0
+        System.out.println("===> sql: "+JpaUtils.toSql(entityManager, User.class, ((root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("name"), "tan")
+        )));
     }
 }
