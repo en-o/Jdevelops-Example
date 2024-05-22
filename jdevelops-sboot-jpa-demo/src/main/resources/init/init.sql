7442b15f4995fe12c2bdf670575b457d3cce46bb
en-o <13320385140@163.com>
2024-05-22 11:25:49 +0800

    删除多余

--- jdevelops-sboot-jpa-demo/src/main/resources/init/init.sql
+++
@@ -1,7 +0,0 @@
-
-
-INSERT INTO `sys_user`(`id`, `address`, `login_name`, `login_pwd`, `name`, `phone`, `user_icon`, `user_no`) VALUES (1,  '重庆', 'admin', '1231', '超级管理员', '15888888888', NULL, 'admin');
-INSERT INTO `sys_user`(`id`, `address`, `login_name`, `login_pwd`, `name`, `phone`, `user_icon`, `user_no`) VALUES (2,  '重庆', 'user', '1234', '用户1', '123', NULL, '1466645430750781440');
-INSERT INTO `sys_user`(`id`, `address`, `login_name`, `login_pwd`, `name`, `phone`, `user_icon`, `user_no`) VALUES (3,  '重庆', 'user02', '123', '用户2', '123', NULL, '1466649744075108352');
-INSERT INTO `sys_user`(`id`, `address`, `login_name`, `login_pwd`, `name`, `phone`, `user_icon`, `user_no`) VALUES (4,  '222', 'SH-01', '123', '111', '', NULL, '1469200870634397696');
-INSERT INTO `sys_user`(`id`, `address`, `login_name`, `login_pwd`, `name`, `phone`, `user_icon`, `user_no`) VALUES (5,  '222', 'LR-01', '1231', '111', '1312', NULL, '1469200914007695360');
