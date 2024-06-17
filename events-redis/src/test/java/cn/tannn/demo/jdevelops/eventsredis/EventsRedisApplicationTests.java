2e100022fbb88c03f8703da55a45c9d4d03ec7d8
en-o <13320385140@163.com>
2024-06-17 10:11:34 +0800

    redis 订阅发布

---
+++ events-redis/src/test/java/cn/tannn/demo/jdevelops/eventsredis/EventsRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tannn.demo.jdevelops.eventsredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
