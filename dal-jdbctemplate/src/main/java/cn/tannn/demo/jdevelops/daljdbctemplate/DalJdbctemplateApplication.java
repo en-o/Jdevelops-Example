877d612fc04735cf75faded2ddd124c01733c2c3
en-o <13320385140@163.com>
2024-06-21 16:15:49 +0800

    temp: test jdbctemplate

---
+++ dal-jdbctemplate/src/main/java/cn/tannn/demo/jdevelops/daljdbctemplate/DalJdbctemplateApplication.java
@@ -0,0 +1,13 @@
+package cn.tannn.demo.jdevelops.daljdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalJdbctemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalJdbctemplateApplication.class, args);
+	}
+
+}
---
+++ dal-jdbctemplate/src/test/java/cn/tannn/demo/jdevelops/daljdbctemplate/DalJdbctemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tannn.demo.jdevelops.daljdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalJdbctemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
