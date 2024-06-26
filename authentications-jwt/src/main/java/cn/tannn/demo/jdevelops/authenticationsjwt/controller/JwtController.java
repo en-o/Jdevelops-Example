3f5a5faca51b8695598fc861093ef0e5b4363ba1
tan <13320385140@163.com>
2024-06-26 16:54:19 +0800

    测试 jwt 5

--- authentications-jwt/src/main/java/cn/tannn/demo/jdevelops/authenticationsjwt/controller/JwtController.java
+++ authentications-jwt/src/main/java/cn/tannn/demo/jdevelops/authenticationsjwt/controller/JwtController.java
@@ -10,6 +10,9 @@ import cn.tannn.jdevelops.result.response.ResultVO;
 import cn.tannn.jdevelops.utils.jwt.core.JwtService;
 import cn.tannn.jdevelops.utils.jwt.module.LoginJwtExtendInfo;
 import cn.tannn.jdevelops.utils.jwt.module.SignEntity;
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import com.github.xiaoymin.knife4j.annotations.ApiSort;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,33 +32,44 @@ public class JwtController {
     @Autowired
     private LoginService loginService;
 
+    @Operation(summary = "拦截")
+    @ApiOperationSupport(order = 1)
     @GetMapping("stop")
     public String stop(){
         return "拦截我";
     }
 
-
+    @Operation(summary = "通过配置放行")
     @GetMapping("letGo")
+    @ApiOperationSupport(order = 2)
     public ResultVO<String> letGo(){
         return ResultVO.success("通过配置放行我");
     }
 
+    @Operation(summary = "通过配置放行2")
+    @ApiOperationSupport(order = 3)
     @GetMapping("letGo2")
     public ReplaceResultVO<String> letGo2(){
         return ReplaceResultVO.success("通过配置放行我");
     }
 
+    @Operation(summary = "利用注解@ApiMapping放行")
+    @ApiOperationSupport(order = 4)
     @ApiMapping(value = "/api/letGo",checkToken = false)
     public String apiLetGo(){
         return "利用注解@ApiMapping放行";
     }
 
-
+    @Operation(summary = "默认放行</user/login>")
+    @ApiOperationSupport(order = 5)
     @GetMapping("/user/login")
     public String defaultLetGo(){
         return "默认放行</user/login>";
     }
 
+
+    @Operation(summary = "login")
+    @ApiOperationSupport(order = 6)
     @GetMapping("/login")
     public TokenSign token(){
         SignEntity<String> signEntity = new SignEntity<>("tan");
@@ -63,13 +77,17 @@ public class JwtController {
         return loginService.login(signEntity);
     }
 
+
+    @Operation(summary = "login2")
+    @ApiOperationSupport(order = 7)
     @ApiMapping(value = "/login2",checkToken = false)
     public TokenSign token2(){
         SignEntity<TestBean> signEntity = new SignEntity<>("tan",new TestBean("hi"));
         return loginService.login(signEntity);
     }
 
-
+    @Operation(summary = "login3")
+    @ApiOperationSupport(order = 8)
     @ApiMapping(value = "/login3",checkToken = false)
     public TokenSign token3(){
         Map<String, String> info = new HashMap<>();
@@ -78,6 +96,8 @@ public class JwtController {
         return loginService.login(signEntity);
     }
 
+    @Operation(summary = "login4")
+    @ApiOperationSupport(order = 9)
     @ApiMapping(value = "/login4", checkToken = false)
     public TokenSign token4() {
         Map<String, String> info = new HashMap<>();
@@ -87,11 +107,16 @@ public class JwtController {
         return loginService.login(signEntity);
     }
 
+    @Operation(summary = "isLogin")
+    @ApiOperationSupport(order = 9)
     @GetMapping("/isLogin")
     public boolean isLogin(HttpServletRequest request){
         return loginService.isLogin(request);
     }
 
+
+    @Operation(summary = "解析token")
+    @ApiOperationSupport(order = 10)
     @GetMapping("/parseJwt")
     public SignEntity<Object> parseJwt(HttpServletRequest request, int type){
         if(type == 0){
@@ -105,7 +130,8 @@ public class JwtController {
         }
     }
 
-
+    @Operation(summary = "subject")
+    @ApiOperationSupport(order = 11)
     @GetMapping("/subject")
     public String subject(HttpServletRequest request){
         String token = JwtWebUtil.getToken(request);
