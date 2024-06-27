302507e7c438715015a1354cec7057039a7a1c77
tan <13320385140@163.com>
2024-06-27 10:54:59 +0800

    测试 jwt 平台端校验

---
+++ authentications-jwt/src/main/java/cn/tannn/demo/jdevelops/authenticationsjwt/controller/Platform2Controller.java
@@ -0,0 +1,29 @@
+package cn.tannn.demo.jdevelops.authenticationsjwt.controller;
+
+import cn.tannn.jdevelops.annotations.web.authentication.ApiPlatform;
+import cn.tannn.jdevelops.annotations.web.constant.PlatformConstant;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 测试 apiPlatform注解
+ *
+ * @author <a href="https://tannn.cn/">tan</a>
+ * @date 2023/10/31 13:46
+ */
+@RestController
+@ApiPlatform(platform = {PlatformConstant.APPLET,PlatformConstant.WEB_ADMIN})
+public class Platform2Controller {
+
+    @GetMapping("classSetting1")
+    public String APPLET_WEB_ADMIN(){
+        return "在类上使用 ApiPlatform APPLET_WEB_ADMIN";
+    }
+
+    @GetMapping("classSetting2")
+    public String APPLET_WEB_ADMIN_2(){
+        return "2在类上使用 ApiPlatform APPLET_WEB_ADMIN";
+    }
+}
---
+++ authentications-jwt/src/main/java/cn/tannn/demo/jdevelops/authenticationsjwt/controller/PlatformController.java
@@ -0,0 +1,57 @@
+package cn.tannn.demo.jdevelops.authenticationsjwt.controller;
+
+import cn.tannn.jdevelops.annotations.web.authentication.ApiMapping;
+import cn.tannn.jdevelops.annotations.web.authentication.ApiPlatform;
+import cn.tannn.jdevelops.annotations.web.constant.PlatformConstant;
+import cn.tannn.jdevelops.jwt.standalone.pojo.TokenSign;
+import cn.tannn.jdevelops.jwt.standalone.service.LoginService;
+import cn.tannn.jdevelops.utils.jwt.module.SignEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 测试 apiPlatform注解
+ *
+ * @author <a href="https://tannn.cn/">tan</a>
+ * @date 2023/10/31 13:46
+ */
+@RestController
+public class PlatformController {
+
+
+    @Autowired
+    private LoginService loginService;
+
+    @ApiMapping(value = "/loginPlatform",checkToken = false, method = RequestMethod.POST)
+    public TokenSign loginPlatform(@RequestBody List<PlatformConstant> platform){
+        SignEntity<String> signEntity = new SignEntity<>("tan", platform);
+        return loginService.login(signEntity);
+    }
+
+    @GetMapping("/no/ApiPlatform")
+    @ApiPlatform()
+    public String NoApiPlatform(){
+        return "NoApiPlatform";
+    }
+
+
+    @GetMapping("/COMMON")
+    @ApiPlatform()
+    public String COMMON(){
+       return "COMMON";
+    }
+
+    @GetMapping("/WEB_ADMIN")
+    @ApiPlatform(platform = {PlatformConstant.WEB_ADMIN})
+    public String WEB_ADMIN(){
+        return "WEB_ADMIN";
+    }
+
+    @GetMapping("/APPLET/WEB_ADMIN")
+    @ApiPlatform(platform = {PlatformConstant.APPLET,PlatformConstant.WEB_ADMIN})
+    public String APPLET_WEB_ADMIN(){
+        return "APPLET_WEB_ADMIN";
+    }
+}
