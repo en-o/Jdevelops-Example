55b03d21a2348bd232e6b2c89159df2a4958b022
tan <13320385140@163.com>
2024-07-05 00:09:38 +0800

    xx

---
+++ frameworks-file/src/main/java/cn/tannn/demo/frameworksfile/test/entity/TestWeb.java
@@ -0,0 +1,31 @@
+package cn.tannn.demo.frameworksfile.test.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author <a href="https://t.tannn.cn/">tnnn</a>
+ * @version V1.0
+ * @date 2024/6/30 上午1:25
+ */
+@Entity
+@Table(name = "test_web")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@DynamicUpdate
+@DynamicInsert
+public class TestWeb extends CommonBean<TestWeb> {
+    String name;
+    String address;
+
+
+}
