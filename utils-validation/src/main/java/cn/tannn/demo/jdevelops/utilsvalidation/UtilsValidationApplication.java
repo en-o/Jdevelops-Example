daae5a33d28ee9439a3db298beae610d48cee2c9
tan <13320385140@163.com>
2024-07-18 13:50:05 +0800

    utilsvalidation 测试 jdk17

--- utils-validation/src/main/java/cn/tannn/demo/jdevelops/utilsvalidation/UtilsValidationApplication.java
+++ utils-validation/src/main/java/cn/tannn/demo/jdevelops/utilsvalidation/UtilsValidationApplication.java
@@ -2,6 +2,7 @@ package cn.tannn.demo.jdevelops.utilsvalidation;
 
 import cn.tannn.demo.jdevelops.utilsvalidation.bean.ResultBean;
 import cn.tannn.demo.jdevelops.utilsvalidation.bean.UserBean;
+import jakarta.validation.Valid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 
 @SpringBootApplication
 @RestController
