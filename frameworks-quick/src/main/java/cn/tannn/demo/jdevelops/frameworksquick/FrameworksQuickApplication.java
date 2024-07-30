9b42f5cf79c8aaa4e33abf0d374da991f55277aa
tan <13320385140@163.com>
2024-07-30 16:45:26 +0800

    test frameworks-quick

---
+++ frameworks-quick/src/main/java/cn/tannn/demo/jdevelops/frameworksquick/FrameworksQuickApplication.java
@@ -0,0 +1,13 @@
+package cn.tannn.demo.jdevelops.frameworksquick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworksQuickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworksQuickApplication.class, args);
+    }
+
+}
