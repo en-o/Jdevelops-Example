1926207c2f7c7e86456fbeb54002729739b0cc1a
tan <13320385140@163.com>
2024-08-06 13:54:06 +0800

    x

--- pf4j-demo/plugins/HelloGreeting/src/main/java/cn/demo/jdevelops/service/HelloMessageProvider.java
+++ pf4j-demo/plugins/HelloGreeting/src/main/java/cn/demo/jdevelops/service/HelloMessageProvider.java
@@ -1,15 +1,28 @@
 package cn.demo.jdevelops.service;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class HelloMessageProvider implements MessageProvider {
 
     @Override
     public String getMessage() {
-        return "hello";
+        return "hello:"+projectVersion();
     }
 
     @Override
     public String getFood() {
-        return "rice";
+        return "rice:"+projectVersion();
+    }
+
+    public String projectVersion() {
+        try {
+            Properties properties = new Properties();
+            properties.load(getClass().getClassLoader().getResourceAsStream("version.properties"));
+            return properties.getProperty("project.version");
+        }catch (Exception e){
+           return "0.0.1";
+        }
     }
 
 }
