0ab1229d951bc7ccd3da133d5143349783348c84
tan <13320385140@163.com>
2024-11-22 13:19:56 +0800

    FluentAPITest.java

--- dal-jpa/src/test/java/cn/tannn/jdevelops/demo/jpa/FluentAPITest.java
+++ dal-jpa/src/test/java/cn/tannn/jdevelops/demo/jpa/FluentAPITest.java
@@ -2,6 +2,7 @@ package cn.tannn.jdevelops.demo.jpa;
 
 import cn.tannn.jdevelops.demo.jpa.dao.UserDao;
 import cn.tannn.jdevelops.demo.jpa.entity.User;
+import cn.tannn.jdevelops.demo.jpa.module.RcUser;
 import cn.tannn.jdevelops.demo.jpa.module.RiUser;
 import cn.tannn.jdevelops.jpa.utils.SpecificationUtil;
 import com.alibaba.fastjson2.JSON;
@@ -12,39 +13,58 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.repository.query.FluentQuery;
 
 /**
  * Fluent API
- * @see https://docs.spring.io/spring-data/jpa/reference/repositories/query-by-example.html#query-by-example.fluent
+ *
  * @author <a href="https://t.tannn.cn/">tan</a>
  * @version V1.0
  * @date 2024/11/22 上午11:58
+ * @see https://docs.spring.io/spring-data/jpa/reference/repositories/query-by-example.html#query-by-example.fluent
  */
 @SpringBootTest
 public class FluentAPITest {
     @Autowired
-   private UserDao userDao;
+    private UserDao userDao;
 
     @Test
-    void testFluentPage(){
+    void testFluentPage() {
         SpecificationUtil<User> instance = SpecificationUtil.getInstance();
-        Specification<User> in = instance.specification((r, q, b)->{
+        Specification<User> in = instance.specification((r, q, b) -> {
             return b.like(r.get("name"), "%用户%");
         });
         Page<User> page = userDao.findBy(in, r -> r.page(Pageable.ofSize(2)));
-        System.out.println("page:"+ JSON.toJSONString(page));
+        System.out.println("page:" + JSON.toJSONString(page));
         page.getContent().forEach(System.out::println);
     }
 
 
     @Test
-    void testFluentFirst(){
+    void testFluentFirst() {
         SpecificationUtil<User> instance = SpecificationUtil.getInstance();
-        Specification<User> in = instance.specification((r, q, b)->{
+        Specification<User> in = instance.specification((r, q, b) -> {
             return b.like(r.get("name"), "%用户%");
         });
         userDao.findBy(in, r -> r.sortBy(Sort.by("name").descending())
                 .first()).ifPresent(System.out::println);
     }
 
+
+    /**
+     * 无效 的 as
+     */
+//    @Test
+//    void testFluentAs() {
+//        SpecificationUtil<User> instance = SpecificationUtil.getInstance();
+//        Specification<User> in = instance.specification((r, q, b) -> {
+//            return b.like(r.get("name"), "%用户%");
+//        });
+//         userDao.findBy(in, r -> r.project("name","address","loginName").as(RcUser.class).all()).forEach(System.out::println);
+//    }
+
+    // 定义投影接口
+    public interface NameOnly {
+        String getName();
+    }
 }
