4f9877cdefd12f1d1cc5fca9b50ceeb078768016
tan <13320385140@163.com>
2024-11-22 12:13:23 +0800

    FluentAPITest

---
+++ dal-jpa/src/test/java/cn/tannn/jdevelops/demo/jpa/FluentAPITest.java
@@ -0,0 +1,50 @@
+package cn.tannn.jdevelops.demo.jpa;
+
+import cn.tannn.jdevelops.demo.jpa.dao.UserDao;
+import cn.tannn.jdevelops.demo.jpa.entity.User;
+import cn.tannn.jdevelops.demo.jpa.module.RiUser;
+import cn.tannn.jdevelops.jpa.utils.SpecificationUtil;
+import com.alibaba.fastjson2.JSON;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+
+/**
+ * Fluent API
+ * @see https://docs.spring.io/spring-data/jpa/reference/repositories/query-by-example.html#query-by-example.fluent
+ * @author <a href="https://t.tannn.cn/">tan</a>
+ * @version V1.0
+ * @date 2024/11/22 上午11:58
+ */
+@SpringBootTest
+public class FluentAPITest {
+    @Autowired
+   private UserDao userDao;
+
+    @Test
+    void testFluentPage(){
+        SpecificationUtil<User> instance = SpecificationUtil.getInstance();
+        Specification<User> in = instance.specification((r, q, b)->{
+            return b.like(r.get("name"), "%用户%");
+        });
+        Page<User> page = userDao.findBy(in, r -> r.page(Pageable.ofSize(2)));
+        System.out.println("page:"+ JSON.toJSONString(page));
+        page.getContent().forEach(System.out::println);
+    }
+
+
+    @Test
+    void testFluentFirst(){
+        SpecificationUtil<User> instance = SpecificationUtil.getInstance();
+        Specification<User> in = instance.specification((r, q, b)->{
+            return b.like(r.get("name"), "%用户%");
+        });
+        userDao.findBy(in, r -> r.sortBy(Sort.by("name").descending())
+                .first()).ifPresent(System.out::println);
+    }
+
+}
