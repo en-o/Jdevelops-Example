db0fe5d6a6f6d078b83b6f09f765ee874fedef5c
tan <13320385140@163.com>
2024-12-12 20:22:28 +0800

    xx

--- dal-jdbctemplate/src/main/java/cn/tannn/demo/jdevelops/daljdbctemplate/DalJdbctemplateApplication.java
+++ dal-jdbctemplate/src/main/java/cn/tannn/demo/jdevelops/daljdbctemplate/DalJdbctemplateApplication.java
@@ -1,24 +1,27 @@
 package cn.tannn.demo.jdevelops.daljdbctemplate;
 
 import cn.tannn.demo.jdevelops.daljdbctemplate.service.DefQueryUserService;
+import cn.tannn.demo.jdevelops.daljdbctemplate.service.QueryUserService;
+import cn.tannn.jdevelops.annotations.jdbctemplate.Query;
+import cn.tannn.jdevelops.jdectemplate.config.JdbcTemplateConfig;
+import cn.tannn.jdevelops.jdectemplate.util.JdbcProxyCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class DalJdbctemplateApplication implements ApplicationRunner {
+public class DalJdbctemplateApplication  {
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(DalJdbctemplateApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(DalJdbctemplateApplication.class, args);
+		QueryUserService queryUserService = run.getBean(QueryUserService.class);
+		queryUserService.findAll().forEach(it -> System.out.printf(it.toString()));
 	}
 
-	private DefQueryUserService defQueryUserService;
 
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-			defQueryUserService.findAll().forEach(it -> System.out.printf(it.toString()));
-	}
 }
