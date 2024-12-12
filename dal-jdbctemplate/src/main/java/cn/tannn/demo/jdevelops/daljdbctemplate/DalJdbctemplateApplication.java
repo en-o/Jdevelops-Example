439427dee25372bde6c7524b16c44207784c65e6
tan <13320385140@163.com>
2024-12-12 16:01:15 +0800

    测试中 .... daljdbctemplate 单侧环境有效，这正常环境无效

--- dal-jdbctemplate/src/main/java/cn/tannn/demo/jdevelops/daljdbctemplate/DalJdbctemplateApplication.java
+++ dal-jdbctemplate/src/main/java/cn/tannn/demo/jdevelops/daljdbctemplate/DalJdbctemplateApplication.java
@@ -1,13 +1,24 @@
 package cn.tannn.demo.jdevelops.daljdbctemplate;
 
+import cn.tannn.demo.jdevelops.daljdbctemplate.service.DefQueryUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DalJdbctemplateApplication {
+public class DalJdbctemplateApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DalJdbctemplateApplication.class, args);
 	}
 
+	private DefQueryUserService defQueryUserService;
+
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+			defQueryUserService.findAll().forEach(it -> System.out.printf(it.toString()));
+	}
 }
