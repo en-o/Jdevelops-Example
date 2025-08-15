56b587c1842232614655fb9b7e51563f2ac6d300
tan <13320385140@163.com>
2025-08-15 10:32:05 +0800

    123

--- dals-renewpwd/src/main/java/cn/tannn/demo/renewpwd/DalsRenewpwdApplication.java
+++ dals-renewpwd/src/main/java/cn/tannn/demo/renewpwd/DalsRenewpwdApplication.java
@@ -1,8 +1,10 @@
 package cn.tannn.demo.renewpwd;
 
 import cn.tannn.jdevelops.renewpwd.PwdCheckDetector;
+import cn.tannn.jdevelops.renewpwd.RenewPwdRefresh;
 import cn.tannn.jdevelops.renewpwd.annotation.EnableRenewpwd;
 import cn.tannn.jdevelops.renewpwd.pojo.PwdExpireInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -13,7 +15,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 @EnableRenewpwd
+@Slf4j
 public class DalsRenewpwdApplication implements ApplicationRunner {
+
 
     @Value("${spring.datasource.username}")
     private String currentUsername;
@@ -24,27 +28,30 @@ public class DalsRenewpwdApplication implements ApplicationRunner {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+
     public static void main(String[] args) {
         SpringApplication.run(DalsRenewpwdApplication.class, args);
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        try(PwdCheckDetector detector =  PwdCheckDetector.builder()
-                // 这里的当前密码可能不是spring.datasource.password，看怎么处理一下
-                .pwdExpireSupplier(()-> new PwdExpireInfo(currentPassword,checkPassword()))
-                .build() ) {
+        log.info("密码续命触发器启动");
+        try {
+            PwdCheckDetector detector = PwdCheckDetector.builder()
+                    // 这里的当前密码可能不是spring.datasource.password，看怎么处理一下
+                    .pwdExpireSupplier(() -> new PwdExpireInfo(currentPassword, checkPassword()))
+                    .retryIntervalMinutes(1) // 探测间隔，默认5分钟
+                    .build();
             // 触发器会自动循环运行
             detector.start();
-        }catch (Exception e){
-            e.printStackTrace();
+        } catch (Exception e) {
+            log.error("密码续命触发器启动失败", e);
         }
         checkPassword();
     }
 
 
-
-    public boolean checkPassword(){
+    public boolean checkPassword() {
         String sql = "SELECT password_expired FROM mysql.user WHERE user = ?";
         String expired = jdbcTemplate.queryForObject(sql, new Object[]{currentUsername}, String.class);
         if ("Y".equalsIgnoreCase(expired)) {
