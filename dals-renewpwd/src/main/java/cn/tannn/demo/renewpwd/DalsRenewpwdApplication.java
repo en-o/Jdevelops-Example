dcada8ecd82f7ee0b147f7d87566a9948a89144a
tan <13320385140@163.com>
2025-08-18 16:31:15 +0800

    123

--- dals-renewpwd/src/main/java/cn/tannn/demo/renewpwd/DalsRenewpwdApplication.java
+++ dals-renewpwd/src/main/java/cn/tannn/demo/renewpwd/DalsRenewpwdApplication.java
@@ -1,7 +1,8 @@
 package cn.tannn.demo.renewpwd;
 
 import cn.tannn.jdevelops.renewpwd.annotation.EnableRenewpwd;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,10 +10,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableRenewpwd
-@Slf4j
 @EnableScheduling
 public class DalsRenewpwdApplication{
 
+    private static final Logger log = LoggerFactory.getLogger(DalsRenewpwdApplication.class);
 
     public static void main(String[] args) {
         log.warn("不用定时器，用异常代替定时器，直接在异常中处理密码过期");
